a25fd90caca2a0940eea378d84e2578275ade75e
AlexINF <alexpro820@gmail.com>
2018-06-09 12:48:07 +0200

    Add parsing methods and DecorationType subclass

--- ZombieProyect-Desktop/Classes/FurnitureType.cs
+++ ZombieProyect-Desktop/Classes/FurnitureType.cs
@@ -1,13 +1,62 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace ZombieProyect_Desktop.Classes
 {
     public class FurnitureType
     {
+        public string textureRef;
+        public Texture2D Texture
+        {
+            get
+            {
+                return Main.furnitureTextures[textureRef];
+            }
+        }
+        public Point TextureSize
+        {
+            get
+            {
+                return Texture.Bounds.Size;
+            }
+        }
+        public FurnitureType(string textureRef)
+        {
+            this.textureRef = textureRef;
+        }
 
+        public class DecorationType : FurnitureType
+        {
+            public DecorationType(string textureRef) : base(textureRef)
+            {
+            }
+
+            public static DecorationType ParseFromXML(XmlNode node)
+            {
+                string _textureRef = node.Attributes["texture"].Value;
+                return new DecorationType(_textureRef);
+            }
+
+            public static DecorationType ParseFromXML(string node)
+            {
+                return ParseFromXML(Main.roomsDocument.SelectSingleNode("/furniture/decoration[@name='" + node + "']"));
+            }
+
+            public static List<DecorationType> GetAllDecorationTypes()
+            {
+                List<DecorationType> types = new List<DecorationType>();
+                foreach (XmlNode n in Main.furnitureDocument.SelectNodes("/furniture/decoration"))
+                {
+                    types.Add(DecorationType.ParseFromXML(n));
+                }
+                return types;
+            }
+        }
     }
 }
