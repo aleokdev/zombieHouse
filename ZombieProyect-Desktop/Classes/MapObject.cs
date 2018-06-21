d0b5a932a13c8ee620707ba6621d562682f6b3a5
AlexINF <alexpro820@gmail.com>
2018-06-21 18:20:18 +0200

    Add MapObject class

---
+++ ZombieProyect-Desktop/Classes/MapObject.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZombieProyect_Desktop.Classes
+{
+    public abstract class MapObject
+    {
+        public Point pos;
+
+        public virtual void Draw() { }
+    }
+}
