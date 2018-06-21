a3cc4d882d0dedcbb9f1f35c11c57d7682935b3a
AlexINF <alexpro820@gmail.com>
2018-06-21 18:29:10 +0200

    Add StorageType, change reference from roomDocument to furnitureDocument

--- ZombieProyect-Desktop/Classes/FurnitureType.cs
+++ ZombieProyect-Desktop/Classes/FurnitureType.cs
@@ -45,13 +45,44 @@ namespace ZombieProyect_Desktop.Classes
 
             public static DecorationType ParseFromXML(string node)
             {
-                return ParseFromXML(Main.roomsDocument.SelectSingleNode("/furniture/decoration[@name='" + node + "']"));
+                return ParseFromXML(Main.furnitureDocument.SelectSingleNode("/furniture/decoration[@name='" + node + "']"));
             }
 
             public static List<DecorationType> GetAllDecorationTypes()
             {
                 List<DecorationType> types = new List<DecorationType>();
                 foreach (XmlNode n in Main.furnitureDocument.SelectNodes("/furniture/decoration"))
+                {
+                    types.Add(ParseFromXML(n));
+                }
+                return types;
+            }
+        }
+
+        public class StorageType : FurnitureType
+        {
+            string openedTextureRef;
+            public StorageType(string textureRef, string openedTextureRef) : base(textureRef)
+            {
+                this.openedTextureRef = openedTextureRef;
+            }
+
+            public static StorageType ParseFromXML(XmlNode node)
+            {
+                string _textureRef = node.Attributes["texture"].Value;
+                string _oTextureRef = node.Attributes["texture_opened"].Value;
+                return new StorageType(_textureRef, _oTextureRef);
+            }
+
+            public static StorageType ParseFromXML(string node)
+            {
+                return ParseFromXML(Main.furnitureDocument.SelectSingleNode("/furniture/storage[@name='" + node + "']"));
+            }
+
+            public static List<StorageType> GetAllStorageTypes()
+            {
+                List<StorageType> types = new List<StorageType>();
+                foreach (XmlNode n in Main.furnitureDocument.SelectNodes("/furniture/storage"))
                 {
                     types.Add(ParseFromXML(n));
                 }
