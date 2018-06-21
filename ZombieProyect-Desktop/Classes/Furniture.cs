2217c87687395f8ccfbb024d97ffc6dbdcf40181
AlexINF <alexpro820@gmail.com>
2018-06-21 19:22:10 +0200

    Change Point pos to Vector2, add Draw override

--- ZombieProyect-Desktop/Classes/Furniture.cs
+++ ZombieProyect-Desktop/Classes/Furniture.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,10 +13,15 @@ namespace ZombieProyect_Desktop.Classes
         public FurnitureType type;
         public Room room;
 
-        public Furniture(Point pos, FurnitureType type, Room r) : base(pos)
+        public Furniture(Vector2 pos, FurnitureType type, Room r) : base(pos)
         {
             this.type = type;
             room = r;
+        }
+
+        public override void Draw(SpriteBatch draw)
+        {
+            draw.Draw(Main.wallpapers[0, 0], new Rectangle((int)pos.X * Main.tileSize, (int)pos.Y * Main.tileSize, 16, 16), Color.White);
         }
     }
 }
