24575bc438727ede1bdb28dfe6abd662f1ce1800
AlexINF <alexpro820@gmail.com>
2018-06-21 16:28:06 +0200

    Add furniture class

---
+++ ZombieProyect-Desktop/Classes/Furniture.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZombieProyect_Desktop.Classes
+{
+    public class Furniture
+    {
+        public Point pos;
+        public FurnitureType type;
+        public Room room;
+
+        public Furniture(Point pos, FurnitureType type, Room r)
+        {
+            this.pos = pos;
+            this.type = type;
+            room = r;
+        }
+    }
+}
