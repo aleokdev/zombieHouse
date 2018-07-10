b32a0329bed29271f765b80308d60d1b3378db0c
AlexINF <alexpro820@gmail.com>
2018-07-10 12:38:53 +0200

    Implement map variable

--- MapManager/Tiles/Door.cs
+++ MapManager/Tiles/Door.cs
@@ -8,8 +8,8 @@ namespace ZombieProyect_Desktop.Classes.Tiles
 {
     public sealed class Door : Wall
     {
-        public Door(int x, int y) : base(x, y) { }
-        public Door(int x, int y, Room parent) : base(x, y, parent) { }
-        
+        public Door(int x, int y, Map parent) : base(x, y, parent) { }
+        public Door(int x, int y, Room parentR, Map parent) : base(x, y, parentR, parent) { }
+
     }
 }
--- MapManager/Tiles/OutsideFloor.cs
+++ MapManager/Tiles/OutsideFloor.cs
@@ -8,8 +8,8 @@ namespace ZombieProyect_Desktop.Classes.Tiles
 {
     public sealed class OutsideFloor : Tile
     {
-        public OutsideFloor(int x, int y) : base(x, y) { }
-        public OutsideFloor(int x, int y, Room parent) : base(x, y, parent) { }
+        public OutsideFloor(int x, int y, Map parent) : base(x, y, parent) { }
+        public OutsideFloor(int x, int y, Room parentR, Map parent) : base(x, y, parentR, parent) { }
 
     }
 }
