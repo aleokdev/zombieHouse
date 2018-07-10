189b8f9681f2afdb65d8810cb30aedcbb048a215
AlexINF <alexpro820@gmail.com>
2018-07-10 12:37:51 +0200

    Implement map variable

--- MapManager/Tiles/Wall.cs
+++ MapManager/Tiles/Wall.cs
@@ -8,21 +8,21 @@ namespace ZombieProyect_Desktop.Classes.Tiles
 {
     public class Wall : Tile
     {
-        public Wall(int x, int y) : base(x, y) { }
-        public Wall(int x, int y, Room parent) : base(x, y, parent) { }
+        public Wall(int x, int y, Map parent) : base(x, y, parent) { }
+        public Wall(int x, int y, Room parentR, Map parent) : base(x, y, parentR, parent) { }
 
         public GridAxis? CheckOuterEdgeOfWall()
         {
-            if ((Map.tileMapSize.X<=Pos.X+1)||(Map.tileMap[Pos.X + 1, Pos.Y]?.GetType() ?? typeof(OutsideFloor)) == typeof(OutsideFloor))
+            if ((parentMap.tileMapSize.X<=Pos.X+1)||(parentMap.tileMap[Pos.X + 1, Pos.Y]?.GetType() ?? typeof(OutsideFloor)) == typeof(OutsideFloor))
                 return GridAxis.positiveX;
 
-            if ((0>Pos.X - 1)||(Map.tileMap[Pos.X - 1, Pos.Y]?.GetType() ?? typeof(OutsideFloor)) == typeof(OutsideFloor))
+            if ((0>Pos.X - 1)||(parentMap.tileMap[Pos.X - 1, Pos.Y]?.GetType() ?? typeof(OutsideFloor)) == typeof(OutsideFloor))
                 return GridAxis.negativeX;
 
-            if ((Map.tileMapSize.Y <= Pos.Y + 1) || (Map.tileMap[Pos.X, Pos.Y + 1]?.GetType() ?? typeof(OutsideFloor)) == typeof(OutsideFloor))
+            if ((parentMap.tileMapSize.Y <= Pos.Y + 1) || (parentMap.tileMap[Pos.X, Pos.Y + 1]?.GetType() ?? typeof(OutsideFloor)) == typeof(OutsideFloor))
                 return GridAxis.positiveY;
 
-            if ((0 > Pos.Y - 1) || (Map.tileMap[Pos.X, Pos.Y - 1]?.GetType() ?? typeof(OutsideFloor)) == typeof(OutsideFloor))
+            if ((0 > Pos.Y - 1) || (parentMap.tileMap[Pos.X, Pos.Y - 1]?.GetType() ?? typeof(OutsideFloor)) == typeof(OutsideFloor))
                 return GridAxis.negativeY;
 
             return null; // Tile is floor and there are no outer edges, or wall is sorrounded by other tiles.
@@ -31,13 +31,13 @@ namespace ZombieProyect_Desktop.Classes.Tiles
         public bool IsCornerWall()
         {
             int a = 0;
-            if ((Map.tileMap[Pos.X + 1, Pos.Y]?.GetType() ?? typeof(OutsideFloor)) == typeof(OutsideFloor))
+            if ((parentMap.tileMap[Pos.X + 1, Pos.Y]?.GetType() ?? typeof(OutsideFloor)) == typeof(OutsideFloor))
                 a++;
-            if ((Map.tileMap[Pos.X - 1, Pos.Y]?.GetType() ?? typeof(OutsideFloor)) == typeof(OutsideFloor))
+            if ((parentMap.tileMap[Pos.X - 1, Pos.Y]?.GetType() ?? typeof(OutsideFloor)) == typeof(OutsideFloor))
                 a++;
-            if ((Map.tileMap[Pos.X, Pos.Y + 1]?.GetType() ?? typeof(OutsideFloor)) == typeof(OutsideFloor))
+            if ((parentMap.tileMap[Pos.X, Pos.Y + 1]?.GetType() ?? typeof(OutsideFloor)) == typeof(OutsideFloor))
                 a++;
-            if ((Map.tileMap[Pos.X, Pos.Y - 1]?.GetType() ?? typeof(OutsideFloor)) == typeof(OutsideFloor))
+            if ((parentMap.tileMap[Pos.X, Pos.Y - 1]?.GetType() ?? typeof(OutsideFloor)) == typeof(OutsideFloor))
                 a++;
 
             if (a > 1) return true;
